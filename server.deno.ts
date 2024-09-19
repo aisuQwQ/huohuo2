b2f45febfd960115daa60cf8b03ba9e9044c7d3e
aisuQwQ <aisuQwQ@ya.ru>
2024-09-19 14:45:36 +0900

    feat: staying min

--- server.deno.ts
+++ server.deno.ts
@@ -1,4 +1,4 @@
-import {createBot, Intents, startBot} from "https://deno.land/x/discordeno@18.0.1/mod.ts";
+import {createBot, startBot} from "https://deno.land/x/discordeno@18.0.1/mod.ts";
 import { ENV } from "./env.ts";
 
 const bot = createBot({
@@ -11,17 +11,18 @@ const bot = createBot({
     },
   });
   
-  // Another way to do events
-  bot.events.messageCreate = function (b, message) {
-    // Process the message here with your command handler.
-  };
+  // bot.events.messageCreate = function (b, message) {
+  //   // Process the message here with your command handler.
+  // };
   
 
 const users: {[key:string]: bigint|undefined}={};
+const times: {[key:string]: number}={};
 
 bot.events.voiceStateUpdate=async (bot, voiceState)=>{
     const userId=voiceState.userId;
     let str:string='';
+    const unixSec=Math.floor(new Date().getTime()/1000);
     //退出
     if(voiceState.channelId==undefined){
         str='退出';
@@ -34,11 +35,25 @@ bot.events.voiceStateUpdate=async (bot, voiceState)=>{
         }
     }
     users[voiceState.userId.toString()]=voiceState.channelId;
-    
+
+    let time='';
+    if(str=='退出'){
+      if(!(userId.toString() in times))times[userId.toString()]=unixSec;
+      time=((unixSec-times[userId.toString()])/60).toFixed(1)+'分';
+    }else{
+      times[userId.toString()]=unixSec;
+    }
+
+
+
     const user=await bot.helpers.getUser(userId);
     bot.helpers.sendMessage('1260178242388623440', {
-        content: user.username+"が"+str+"しました"
+        content: user.username+"が"+str+"しました "+time
     })
 }
 
 await startBot(bot);
+
+setInterval(()=>{
+  console.log('a');
+}, 60*1000)
